6d2c0e6aaf0eb2bd43aa9d54a6f01324840532f4
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-11-07 12:17:27 +0330

    add Tree

---
+++ Abstract_Factory/src/main/java/Tree.java
@@ -0,0 +1,11 @@
+public abstract class Tree {
+    private int length;
+
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    public int getLength(){
+        return length;
+    }
+}
