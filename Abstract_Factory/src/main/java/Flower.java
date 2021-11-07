00b2a764788ac693fff9a6803e5697e5edcaed01
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-11-07 12:21:09 +0330

    add Flower

---
+++ Abstract_Factory/src/main/java/Flower.java
@@ -0,0 +1,11 @@
+public abstract class Flower {
+    private int length;
+
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    public int getLength(){
+        return length;
+    }
+}
